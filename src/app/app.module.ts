5e543d906fece2d17abec2cb8795508131bd558e
gibers <chbaptis@hotmail.fr>
2023-05-21 22:29:35 +0200

    first commit.

---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+import { APP_INITIALIZER, NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
+import { KeycloakAngularModule, KeycloakService } from 'keycloak-angular';
+
+function initializeKeycloak(keycloak: KeycloakService) {
+  return () =>
+    keycloak.init({
+      config: {
+        url: 'http://localhost:8080',
+        realm: 'myRealm',
+        clientId: 'myclient'
+      },
+      initOptions: {
+        onLoad: 'check-sso',
+        silentCheckSsoRedirectUri:
+          window.location.origin + '/assets/silent-check-sso.html'
+      }
+    });
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    WelcomePageComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    KeycloakAngularModule
+  ],
+  providers: [{
+    provide: APP_INITIALIZER,
+    useFactory: initializeKeycloak,
+    multi: true,
+    deps: [KeycloakService]
+  }],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
